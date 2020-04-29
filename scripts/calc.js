4a3cfcfc5e0e71cb0d2de00a2b5e44ade40571f3
Machiko Lacey-Kimura <machiko_kimura_lacey@yahoo.co.uk>
2020-04-29 21:44:02 +0000

    Test alert

--- scripts/calc.js
+++ scripts/calc.js
@@ -1,11 +1,11 @@
-Calculator = function(){
+Calculator = function() {
     this.value = 0;
-}
+};
 
-Calculator.prototype.add = function(number){
-    if(typeof(number) == "number"){
+Calculator.prototype.add = function(number) {
+    if (typeof(number) == "number") {
         this.value += number;
-    }else{
-        alert('Error!');
+    } else {
+        alert("Error!");
     }
-}
+};
